5243de1c8acd0d066d74df0acededd6c9577f27f
borrascador <roflbot101@gmail.com>
2017-07-10 15:36:11 -0400

    Changed key and separated key from settings file

--- marksite/marksite/settings/base.py
+++ marksite/marksite/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+wyp(j4vo!-)5^1a!7sf+avet&7$u!-x0fmyh^$j(chd=rbq65'
+from secret import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
