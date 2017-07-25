2ba34fddf8bb2b6a474ab56352be797559f81e41
borrascador <roflbot101@gmail.com>
2017-07-25 23:24:37 +0200

    Random candle colors and organized specs images

--- marksite/donate/views.py
+++ marksite/donate/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import random
 
 from django.views.generic.base import TemplateView
 
@@ -9,3 +10,10 @@ class DonateView(TemplateView):
     
     def loop_times(self):
         return range(1,21)
+    
+    def choose_candle(self):
+        candles = [
+            '/images/candle-blue.png',
+            '/images/candle-red.png',
+        ]
+        return random.choice(candles)
