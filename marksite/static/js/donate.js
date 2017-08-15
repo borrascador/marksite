e8a808c0e677efaba9e22c888ac9621ef4347911
borrascador <roflbot101@gmail.com>
2017-08-14 18:19:06 -0700

    general update to demo for mark

--- marksite/static/js/donate.js
+++ marksite/static/js/donate.js
@@ -1,26 +1,27 @@
-var candle_list = [];
+var candleList = [];
 var counter = 0;
 
 function lightCandle(maximum) {
     var minimum = 1;
     var maximum = maximum;
     do {
-        var random_number = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-    } while (candle_list[random_number] && counter < maximum)
+        var randomNumber = 
+        Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    } while (candleList[randomNumber] && counter < maximum)
     
-    var candle_id = "candle".concat("_", random_number.toString());
-    candle_list[random_number] = candle_id;
+    var candleId = "candle".concat("_", randomNumber.toString());
+    candleList[randomNumber] = candleId;
     counter++;
-    document.getElementById(candle_id).style.opacity = "1";
+    document.getElementById(candleId).style.opacity = "1";
 }
 
 function clearCandles(maximum) {
     var maximum = maximum;
     for (var i = 1; i < maximum + 1; i++) {
-        if (candle_list[i]) {
-            document.getElementById(candle_list[i]).style.opacity = "0";
-        }    
+        if (candleList[i]) {
+            document.getElementById(candleList[i]).style.opacity = "0";
+        }
     }
-    candle_list = [];
-    counter = 0;    
+    candleList = [];
+    counter = 0;
 }
